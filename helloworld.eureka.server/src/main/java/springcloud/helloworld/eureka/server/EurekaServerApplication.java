a2bdd66bf5a1a2d75bc72f8233ff04ad6fd52f1f
sam <qinlf@shengyouworld.com>
2018-08-22 18:43:35 +0800

    2

--- helloworld.eureka.server/src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
+++ helloworld.eureka.server/src/main/java/springcloud/helloworld/eureka/server/EurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
  
+	
      public static void main(String[] args) {
          SpringApplication.run(EurekaServerApplication.class, args);
      }

c36543bc4fa5f98e37ba556956e3918166089bd3
sam <qinlf@shengyouworld.com>
2018-08-23 11:00:59 +0800

    ss

--- helloworld.eureka.client/src/main/java/springcloud/helloworld/eureka/client/EurekaClientApplication.java
+++ helloworld.eureka.client/src/main/java/springcloud/helloworld/eureka/client/EurekaClientApplication.java
@@ -24,6 +24,8 @@ public class EurekaClientApplication {
     public String home() {
     	
     	System.out.println(123);
+    	
+    	System.out.println(125);
 
         return "hello world from port " + port;     
     }
